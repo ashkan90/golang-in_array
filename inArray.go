5e02ea76404fdb5e43b5c95163593a20ecec4792
ashkan90 <emirhan.1ataman@gmail.com>
2019-08-22 18:26:00 +0300

    fixed bugs. deep control added.

--- inArray.go
+++ inArray.go
@@ -2,9 +2,10 @@ package exists
 
 import (
 	"reflect"
+	"strings"
 )
 
-func In_array(search interface{}, array interface{}) bool {
+func In_array(search interface{}, array interface{}, deep bool) bool {
 
 	val := reflect.ValueOf(array)
 	val = val.Convert(val.Type())
@@ -18,8 +19,15 @@ func In_array(search interface{}, array interface{}) bool {
 		for s.Next() {
 			s.Value().Convert(s.Value().Type())
 			for i := 0; i < s.Value().Len(); i++ {
-				if reflect.DeepEqual(search, s.Value().Index(i).Interface()) {
-					return true
+				if deep {
+					if reflect.DeepEqual(search, s.Value().Index(i).Interface()) {
+						return true
+					}
+				} else {
+					str := s.Value().Index(i).String()
+					if strings.Contains(str, search.(string)) {
+						return true
+					}
 				}
 			}
 		}
